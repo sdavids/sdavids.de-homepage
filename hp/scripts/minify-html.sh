8e73e267ab324109989769c344d72b4b0b741fc5
Sebastian Davids <sdavids@gmx.de>
2022-01-20 16:03:12 +0100

    chore: minify html
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/scripts/minify-html.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+readonly dir="dist"
+readonly file="${dir}/index.html"
+
+npx html-minifier "${file}" \
+  --collapse-boolean-attributes \
+  --collapse-whitespace \
+  --html5 \
+  --minify-css \
+  --remove-comments \
+  --remove-empty-attributes \
+  --remove-redundant-attributes \
+  --remove-script-type-attributes \
+  --remove-style-link-type-attributes \
+  --use-short-doctype \
+  -o "${file}.min"
+
+mv "${file}.min" "${file}"
