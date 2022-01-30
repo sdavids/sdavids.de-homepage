439a5b6678a598ad08cb29868518041323da0be1
Sebastian Davids <sdavids@gmx.de>
2022-01-30 12:43:06 +0100

    chore: add clean build task
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/scripts/clean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# script needs to be invoked from the hp root directory
+
+set -eu
+
+readonly base_dir="$(pwd)"
+
+readonly build_dir="${base_dir}/dist"
+readonly eslint_cache_file="${base_dir}/.eslintcache"
+readonly generated_app_css_file="${base_dir}/src/s/app.css"
+
+rm -rf "${build_dir}" \
+        "${eslint_cache_file}" \
+       "${generated_app_css_file}"
+
