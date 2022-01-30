1d4f0ff09a507c28eb229a4f18ee0924e9a756d5
Sebastian Davids <sdavids@gmx.de>
2022-01-30 11:54:20 +0100

    chore: add hash:filename build task
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/scripts/hash-filename.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if [[ -z "$*" ]]; then echo "Usage: $0 FILE"; exit 1; fi
+
+readonly file="$1"
+readonly echo="${2:-}"
+
+if [[ ! -f "${file}" ]]; then echo "${file} does not exist"; exit 2; fi
+
+readonly hash=($(sha1sum "${file}" | cut -c 1-7))
+readonly filename=$(rev <<< "${file}" | cut -d "." -f2- | rev)
+readonly extension="${1##*.}"
+
+if [[ "${filename}" == "${extension}" ]]; then
+  filename_hashed="${filename}.${hash[0]}"
+  mv "${file}" "${filename_hashed}"
+else
+  filename_hashed="${filename}.${hash[0]}.${extension}"
+  mv "${file}" "${filename_hashed}"
+fi
+
+if [[ "${echo}" = "-e" ]]; then echo "${filename_hashed}"; fi
