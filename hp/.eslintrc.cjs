2be1b447fc294766639c67f26c0fe919e7c4b8bf
Sebastian Davids <sdavids@gmx.de>
2022-02-07 18:50:57 +0100

    chore: set ES2017 target
    https://web.dev/publish-modern-javascript/
    
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/.eslintrc.cjs
+++ hp/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es6: true,
+    es2017: true,
   },
   parserOptions: {
     sourceType: 'module',
--- hp/scripts/build.sh
+++ hp/scripts/build.sh
@@ -6,6 +6,7 @@ npm run build:css
 
 rm -rf dist
 cp -rp src dist
-npx esbuild src/j/app.js --bundle --splitting --outdir=dist/j --format=esm --target=es6 --minify
+npx esbuild src/j/app.js --bundle --splitting --outdir=dist/j --format=esm --target=es2017 --minify
+# npx esbuild src/j/app.js --bundle --outfile=dist/j/app.es5.js --format=iife --target=es5 --minify
 
 rm dist/s/app.src.css
