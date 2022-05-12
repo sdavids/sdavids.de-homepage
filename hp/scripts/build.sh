a18a6e0ace77bd530d9227668229063c5483f2b3
Sebastian Davids <sdavids@gmx.de>
2022-05-12 14:08:50 +0200

    chore: do not emit legal comments
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/build.sh
+++ hp/scripts/build.sh
@@ -8,7 +8,8 @@ npm run build:css
 
 rm -rf dist
 cp -rp src dist
-npx esbuild src/j/app.js --bundle --splitting --outdir=dist/j --format=esm --target=es2017 --minify
-# npx esbuild src/j/app.js --bundle --outfile=dist/j/app.es5.js --format=iife --target=es5 --minify
+
+npx esbuild src/j/app.js --bundle --splitting --outdir=dist/j --format=esm --target=es2017 --minify --legal-comments=none
+# npx esbuild src/j/app.js --bundle --outfile=dist/j/app.es5.js --format=iife --target=es5 --minify --legal-comments=none
 
 rm dist/s/app.src.css
