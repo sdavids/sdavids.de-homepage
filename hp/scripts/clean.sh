4be460cad2d4fb755de4a4dcd1929eaf3dc067de
Sebastian Davids <sdavids@gmx.de>
2023-08-30 11:29:42 +0200

    chore: update prettier to 2.8.8
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/clean.sh
+++ hp/scripts/clean.sh
@@ -8,8 +8,10 @@ readonly base_dir="$(pwd)"
 
 readonly build_dir="${base_dir}/dist"
 readonly eslint_cache_file="${base_dir}/.eslintcache"
+readonly prettier_cache_file="${base_dir}/node_modules/.cache/prettier/.prettier-cache"
 readonly generated_app_css_file="${base_dir}/src/s/app.css"
 
 rm -rf "${build_dir}" \
        "${eslint_cache_file}" \
+       "${prettier_cache_file}" \
        "${generated_app_css_file}"
