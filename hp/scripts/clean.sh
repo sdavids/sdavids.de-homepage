cb3694fa55069926e835b156733e3ebe38cb8ccd
Sebastian Davids <sdavids@gmx.de>
2023-08-30 11:24:13 +0200

    chore: cleanup script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/clean.sh
+++ hp/scripts/clean.sh
@@ -11,6 +11,5 @@ readonly eslint_cache_file="${base_dir}/.eslintcache"
 readonly generated_app_css_file="${base_dir}/src/s/app.css"
 
 rm -rf "${build_dir}" \
-        "${eslint_cache_file}" \
+       "${eslint_cache_file}" \
        "${generated_app_css_file}"
-
