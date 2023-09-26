67da27f7f36164a81f6dde5bdbcbed0591aa9b25
Sebastian Davids <sdavids@gmx.de>
2023-09-26 13:09:57 +0200

    build: add hash to JS and CSS filenames
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/build.sh
+++ hp/scripts/build.sh
@@ -14,3 +14,14 @@ npx esbuild src/j/app.js --bundle --splitting --outdir=dist/j --format=esm --tar
 npx esbuild --bundle dist/s/app.css --outfile=dist/s/app.css --allow-overwrite --minify --legal-comments=none
 
 rm dist/s/app.src.css
+
+readonly js_filename="$(npm run hash:filename -- dist/j/app.js -e | sed -nr "s/.*(app.[0-9a-f]+\.js)/\1/p")"
+readonly css_filename="$(npm run hash:filename -- dist/s/app.css -e| sed -nr "s/.*(app.[0-9a-f]+\.css)/\1/p")"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed -i '' "s/app\.js/${js_filename}/g" dist/index.html
+  sed -i '' "s/app\.css/${css_filename}/g" dist/index.html
+else
+  sed -i "s/app\.js/${js_filename}/g" dist/index.html
+  sed -i "s/app\.css/${css_filename}/g" dist/index.html
+fi
