0e59c4ef34742d63a875432b739c095c123ec916
Sebastian Davids <sdavids@gmx.de>
2023-10-02 11:09:18 +0200

    chore: cleanup build file
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/build.sh
+++ hp/scripts/build.sh
@@ -4,24 +4,28 @@
 
 set -eu
 
+readonly dir="dist"
+
 npm run build:css
 
-rm -rf dist
-cp -rp src dist
+rm -rf "${dir}"
+cp -rp src "${dir}"
 
-npx esbuild src/j/app.js --bundle --splitting --outdir=dist/j --format=esm --target=es2017 --minify --legal-comments=none
+npx esbuild "src/j/app.js" --bundle --splitting --outdir="${dir}/j" --format=esm --target=es2017 --minify --legal-comments=none
 
-npx esbuild --bundle dist/s/app.css --outfile=dist/s/app.css --allow-overwrite --minify --legal-comments=none
+npx esbuild --bundle "${dir}/s/app.css" --outfile="${dir}/s/app.css" --allow-overwrite --minify --legal-comments=none
 
-rm dist/s/app.src.css
+rm "${dir}/s/app.src.css"
 
 readonly js_filename="$(npm run hash:filename -- dist/j/app.js -e | sed -nr "s/.*(app.[0-9a-f]+\.js)/\1/p")"
 readonly css_filename="$(npm run hash:filename -- dist/s/app.css -e| sed -nr "s/.*(app.[0-9a-f]+\.css)/\1/p")"
 
+readonly index_file="${dir}/index.html"
+
 if [[ "$(uname)" == "Darwin" ]]; then
-  sed -i '' "s/app\.js/${js_filename}/g" dist/index.html
-  sed -i '' "s/app\.css/${css_filename}/g" dist/index.html
+  sed -i '' "s/app\.js/${js_filename}/g" "${index_file}"
+  sed -i '' "s/app\.css/${css_filename}/g" "${index_file}"
 else
-  sed -i "s/app\.js/${js_filename}/g" dist/index.html
-  sed -i "s/app\.css/${css_filename}/g" dist/index.html
+  sed -i "s/app\.js/${js_filename}/g" "${index_file}"
+  sed -i "s/app\.css/${css_filename}/g" "${index_file}"
 fi
