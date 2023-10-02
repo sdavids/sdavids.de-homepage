7b250360d57c0e047d64e3a0e75d93b84661a6ab
Sebastian Davids <sdavids@gmx.de>
2023-10-02 11:07:12 +0200

    chore: minify webmanifest
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/minify-json.sh
+++ hp/scripts/minify-json.sh
@@ -12,3 +12,10 @@ readonly traffic_advice_tmp_file="${traffic_advice_file}.tmp"
 mv "${traffic_advice_file}" "${traffic_advice_tmp_file}"
 jq -c . "${traffic_advice_tmp_file}" > "${traffic_advice_file}"
 rm "${traffic_advice_tmp_file}"
+
+readonly webmanifest_file="${dir}/site.webmanifest"
+readonly webmanifest_tmp_file="${traffic_advice_file}.tmp"
+
+mv "${webmanifest_file}" "${webmanifest_tmp_file}"
+jq -c . "${webmanifest_tmp_file}" > "${webmanifest_file}"
+rm "${webmanifest_tmp_file}"
