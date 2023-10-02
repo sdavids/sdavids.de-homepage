345631c385ba47cddf5ea3c4b5854609a546fddf
Sebastian Davids <sdavids@gmx.de>
2023-10-02 11:12:11 +0200

    chore: minify robots.txt
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/scripts/minify-robots.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# script needs to be invoked from the hp root directory
+
+set -eu
+
+readonly dir="dist"
+readonly file="${dir}/robots.txt"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed -i '' "/^[@#]/ d" "${file}"
+else
+  sed -i "/^[@#]/ d" "${file}"
+fi
