c192ae2afdc19d9c7bc0a2a0b7e30bc1b2e65616
Sebastian Davids <sdavids@gmx.de>
2023-10-24 22:31:12 +0200

    chore: add npm cleanup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/clean.sh
+++ hp/scripts/clean.sh
@@ -9,9 +9,13 @@ readonly base_dir="${PWD}"
 readonly build_dir="${base_dir}/dist"
 readonly eslint_cache_file="${base_dir}/.eslintcache"
 readonly prettier_cache_file="${base_dir}/node_modules/.cache/prettier/.prettier-cache"
+readonly npm_logs_dir="${base_dir}/_logs"
+readonly npm_cache_dir="${base_dir}/_cacache"
 readonly generated_app_css_file="${base_dir}/src/s/app.css"
 
 rm -rf "${build_dir}" \
        "${eslint_cache_file}" \
        "${prettier_cache_file}" \
+       "${npm_logs_dir}" \
+       "${npm_cache_dir}" \
        "${generated_app_css_file}"
