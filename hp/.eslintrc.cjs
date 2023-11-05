368719d315f0a362386964824d167aef56ecff63
Sebastian Davids <sdavids@gmx.de>
2023-11-05 01:08:10 +0100

    chore: update eslint config
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/.eslintrc.cjs
+++ hp/.eslintrc.cjs
@@ -21,10 +21,12 @@ module.exports = {
   reportUnusedDisableDirectives: true,
   rules: {
     'capitalized-comments': 'off',
+    'func-names': ['error', 'always', { generators: 'as-needed' }],
     'id-length': 'off',
     'sort-keys': 'off',
     'multiline-comment-style': 'off',
     'no-magic-numbers': 'off',
+    'no-ternary': 'off',
     'one-var': 'off',
     'prefer-destructuring': ['error', { object: true, array: false }],
     'sort-vars': 'off',
--- hp/eslint.config.js
+++ hp/eslint.config.js
@@ -29,10 +29,12 @@ export default [
     },
     rules: {
       'capitalized-comments': 'off',
+      'func-names': ['error', 'always', { generators: 'as-needed' }],
       'id-length': 'off',
       'sort-keys': 'off',
       'multiline-comment-style': 'off',
       'no-magic-numbers': 'off',
+      'no-ternary': 'off',
       'one-var': 'off',
       'prefer-destructuring': ['error', { object: true, array: false }],
       'sort-vars': 'off',
