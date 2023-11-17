b26c23eb426894aa3bf081804649b11fdb624c24
Sebastian Davids <sdavids@gmx.de>
2023-11-17 11:25:34 +0100

    chore: add commitlint
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/.commitlintrc.cjs
@@ -0,0 +1,10 @@
+// https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0, 'never'],
+    'trailer-exists': [2, 'always', 'Signed-off-by:'],
+    'body-max-line-length': [0, 'never'],
+  },
+};
