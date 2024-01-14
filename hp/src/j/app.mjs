ca67ab9b2d2fe209a9864a362ce4cb05cee5d015
Sebastian Davids <sdavids@gmx.de>
2024-01-14 23:37:17 +0100

    chore: use sebastian@sdavids.de for email
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/src/j/app.mjs
+++ hp/src/j/app.mjs
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2022-2023, Sebastian Davids
+ Copyright (c) 2022-2024, Sebastian Davids
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -13,7 +13,3 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-document.addEventListener('DOMContentLoaded', () => {
-  const a = document.getElementById('sd-contact');
-  a.href = atob(a.dataset.h);
-});
