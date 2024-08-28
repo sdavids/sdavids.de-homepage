6de58557690b1daa7c91e1f07bc01511034ffdf1
Sebastian Davids <sdavids@gmx.de>
2024-08-28 10:01:26 +0200

    chore: improve dependency-check-node script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/dependency-check-node.sh
+++ hp/scripts/dependency-check-node.sh
@@ -6,11 +6,19 @@
 set -eu
 
 readonly base_dir="${1:-$PWD}"
+readonly ignore_scripts="${2:-true}"
 
-cd "${base_dir}"
+if [ "${base_dir}" != "$PWD" ] && [ "${base_dir}" != '.' ]; then
+  cd "${base_dir}"
+  if command -v fnm >/dev/null 2>&1; then
+    fnm use
+  elif command -v nvm >/dev/null 2>&1; then
+    nvm use
+  fi
+fi
 
 if [ ! -d 'node_modules' ]; then
-  npm ci --ignore-scripts=false --fund=false
+  npm ci --ignore-scripts="${ignore_scripts}" --fund=false
 fi
 
 npm outdated --long
