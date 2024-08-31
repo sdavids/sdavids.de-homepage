d1cb4730f8ee08ab36fb89de94fca875f8fba4eb
Sebastian Davids <sdavids@gmx.de>
2024-08-31 11:37:32 +0200

    chore: add clean-node script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/scripts/clean-node.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+rm -rf "${base_dir}/node_modules" "${base_dir}/package-lock.json"
