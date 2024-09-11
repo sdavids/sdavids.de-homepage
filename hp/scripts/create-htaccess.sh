7df81c317daa8d589efa4a639581596de276c4d0
Sebastian Davids <sdavids@gmx.de>
2024-09-11 16:33:31 +0200

    chore: remove unnecessary comment
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/create-htaccess.sh
+++ hp/scripts/create-htaccess.sh
@@ -3,8 +3,6 @@
 # SPDX-FileCopyrightText: © 2022 Sebastian Davids <sdavids@gmx.de>
 # SPDX-License-Identifier: Apache-2.0
 
-# script needs to be invoked from the hp root directory
-
 set -Eeu -o pipefail -o posix
 
 readonly base_dir="${1:-$PWD}"
--- hp/scripts/create-security-txt-file.sh
+++ hp/scripts/create-security-txt-file.sh
@@ -3,8 +3,6 @@
 # SPDX-FileCopyrightText: © 2023 Sebastian Davids <sdavids@gmx.de>
 # SPDX-License-Identifier: Apache-2.0
 
-# script needs to be invoked from the hp root directory
-
 # gpg needs to be in $PATH
 #   Mac:
 #     https://gpgtools.org
