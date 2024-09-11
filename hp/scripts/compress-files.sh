88c24525201acb3fd52155907264090448ba522d
Sebastian Davids <sdavids@gmx.de>
2024-09-11 16:25:42 +0200

    chore: improve compress-files script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/compress-files.sh
+++ hp/scripts/compress-files.sh
@@ -3,8 +3,6 @@
 # SPDX-FileCopyrightText: © 2022 Sebastian Davids <sdavids@gmx.de>
 # SPDX-License-Identifier: Apache-2.0
 
-# script needs to be invoked from the hp root directory
-
 set -eu
 
 if [ -z "$*" ]; then
@@ -17,8 +15,6 @@ if [ ! -d "$1" ]; then
   exit 2
 fi
 
-readonly dir="$PWD/$1"
-
-find "${dir}" \( -name '*.age' -o -name '*.css' -o -name '*.gpg' -o -name '*.html' -o -name '*.mjs' -o -name '*.keys' -o -name 'site.webmanifest' -o -name 'sitemap.xml' -o -name '*.svg' -type f \) -exec scripts/compress-zstd.sh {} ';'
-find "${dir}" \( -name '*.age' -o -name '*.css' -o -name '*.gpg' -o -name '*.html' -o -name '*.mjs' -o -name '*.keys' -o -name 'site.webmanifest' -o -name 'sitemap.xml' -o -name '*.svg' -type f \) -exec scripts/compress-brotli.sh {} ';'
-find "${dir}" \( -name '*.age' -o -name '*.css' -o -name '*.gpg' -o -name '*.html' -o -name '*.mjs' -o -name '*.keys' -o -name 'site.webmanifest' -o -name 'sitemap.xml' -o -name '*.svg' -type f \) -exec scripts/compress-gzip.sh {} ';'
+find "$1" \( -name '*.age' -o -name '*.css' -o -name '*.gpg' -o -name '*.html' -o -name '*.mjs' -o -name '*.keys' -o -name 'site.webmanifest' -o -name 'sitemap.xml' -o -name '*.svg' -type f \) -exec scripts/compress-zstd.sh {} ';'
+find "$1" \( -name '*.age' -o -name '*.css' -o -name '*.gpg' -o -name '*.html' -o -name '*.mjs' -o -name '*.keys' -o -name 'site.webmanifest' -o -name 'sitemap.xml' -o -name '*.svg' -type f \) -exec scripts/compress-brotli.sh {} ';'
+find "$1" \( -name '*.age' -o -name '*.css' -o -name '*.gpg' -o -name '*.html' -o -name '*.mjs' -o -name '*.keys' -o -name 'site.webmanifest' -o -name 'sitemap.xml' -o -name '*.svg' -type f \) -exec scripts/compress-gzip.sh {} ';'
