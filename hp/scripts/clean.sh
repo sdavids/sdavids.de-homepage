08400e781959c637311bb05409e920558bd038de
Sebastian Davids <sdavids@gmx.de>
2025-03-15 19:59:58 +0100

    chore: clean tsBuildInfoFile
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/clean.sh
+++ hp/scripts/clean.sh
@@ -10,6 +10,7 @@ readonly base_dir="${1:-$PWD}"
 readonly build_dir="${base_dir}/dist"
 readonly eslint_cache_file="${base_dir}/.eslintcache"
 readonly prettier_cache_file="${base_dir}/node_modules/.cache/prettier/.prettier-cache"
+readonly ts_build_info_file="${base_dir}/node_modules/.cache/tsc/.tsbuildcache"
 readonly generated_app_css_file="${base_dir}/src/s/app.css"
 readonly lighthouseci_dir="${base_dir}/.lighthouseci"
 readonly playwright_test_results_dir="${base_dir}/test-results"
@@ -18,6 +19,7 @@ readonly playwright_reports_dir="${base_dir}/playwright-report"
 rm -rf "${build_dir}" \
   "${eslint_cache_file}" \
   "${prettier_cache_file}" \
+  "${ts_build_info_file}" \
   "${generated_app_css_file}" \
   "${lighthouseci_dir}" \
   "${playwright_test_results_dir}" \
