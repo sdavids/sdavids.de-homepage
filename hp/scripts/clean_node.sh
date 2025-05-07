bbba21878aa2b7a7af4293c14ac1c0e0d2f87ecb
Sebastian Davids <sdavids@gmx.de>
2025-05-07 17:39:36 +0200

    add -k and -r to clean_node
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/clean_node.sh
+++ hp/scripts/clean_node.sh
@@ -5,6 +5,39 @@
 
 set -eu
 
-readonly base_dir="${1:-$PWD}"
+while getopts ':kr' opt; do
+  case "${opt}" in
+    k)
+      keep_lock='true'
+      ;;
+    r)
+      recurse='true'
+      ;;
+    ?)
+      echo "Usage: $0 [-k] [-r] <dir>" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
 
-rm -rf "${base_dir}/node_modules" "${base_dir}/package-lock.json"
+readonly base_dir="${1:-$PWD}"
+readonly recurse="${recurse:-false}"
+readonly keep_lock="${keep_lock:-false}"
+
+if [ ! -d "${base_dir}" ]; then
+  printf "The directory '%s' does not exist.\n" "${base_dir}" >&2
+  exit 2
+fi
+
+if [ "${recurse}" = 'true' ]; then
+  find "${base_dir}" -type d -name node_modules -exec rm -rf {} +
+  if [ "${keep_lock}" = 'false' ]; then
+    find "${base_dir}" -type f -name package-lock.json -exec rm -f {} +
+  fi
+else
+  rm -rf "${base_dir}/node_modules"
+  if [ "${keep_lock}" = 'false' ]; then
+    rm -f "${base_dir}/package-lock.json"
+  fi
+fi
