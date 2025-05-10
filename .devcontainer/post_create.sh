d5110545327f778d47acc229882a430e4e531268
Sebastian Davids <sdavids@gmx.de>
2025-05-10 21:07:17 +0200

    non-silent install in devcontainer
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,7 +5,7 @@
 
 bash -i -c 'export SHELL=bash && curl -fsSL https://get.pnpm.io/install.sh | sh -'
 
-bash -i -c 'cd hp && pnpm env use --global "$(cat .nvmrc)" && rm -rf node_modules && npm i --silent --ignore-scripts=false --fund=false --audit=false && export DEBIAN_FRONTEND=noninteractive && npx playwright install --with-deps && npm cache clean --force >/dev/null 2>&1'
+bash -i -c 'cd hp && pnpm env use --global "$(cat .nvmrc)" && rm -rf node_modules && npm i --ignore-scripts=false --fund=false --audit=false && export DEBIAN_FRONTEND=noninteractive && npx playwright install --with-deps && npm cache clean --force >/dev/null 2>&1'
 
 sudo apt-get autoremove -y
 sudo apt-get clean -y
