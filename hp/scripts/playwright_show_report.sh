479790c831f53f4d36e80a6e77bb89fcc0c84d92
Sebastian Davids <sdavids@gmx.de>
2025-05-10 21:08:16 +0200

    add playwright scripts
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ hp/scripts/playwright_show_report.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# https://github.com/devcontainers/features/tree/main/src/docker-outside-of-docker#1-use-the-localworkspacefolder-as-environment-variable-in-your-code
+if [ -n "${LOCAL_WORKSPACE_FOLDER+x}" ]; then
+  npx --yes --quiet playwright show-report --host 0.0.0.0 reports/playwright
+else
+  npx --yes --quiet playwright show-report reports/playwright
+fi
---
+++ hp/scripts/playwright_smoke.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# https://github.com/devcontainers/features/tree/main/src/docker-outside-of-docker#1-use-the-localworkspacefolder-as-environment-variable-in-your-code
+if [ -n "${LOCAL_WORKSPACE_FOLDER+x}" ]; then
+  export PLAYWRIGHT_HTML_HOST=0.0.0.0
+fi
+
+npx --yes --quiet playwright test --project smoke
