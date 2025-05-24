91029aed0614bc9b05309ccbd8ce0f65c54902f2
Sebastian Davids <sdavids@gmx.de>
2025-05-24 21:22:14 +0200

    delete cache directories instead of files
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- hp/scripts/clean.sh
+++ hp/scripts/clean.sh
@@ -8,18 +8,18 @@ set -eu
 readonly base_dir="${1:-$PWD}"
 
 readonly build_dir="${base_dir}/dist"
-readonly prettier_cache_file="${base_dir}/node_modules/.cache/prettier/.prettier-cache"
-readonly ts_build_info_file="${base_dir}/node_modules/.cache/tsc/.tsbuildcache"
 readonly eslint_cache_dir="${base_dir}/node_modules/.cache/eslint"
+readonly prettier_cache_dir="${base_dir}/node_modules/.cache/prettier"
+readonly ts_build_cache_dir="${base_dir}/node_modules/.cache/tsc"
 readonly generated_app_css_file="${base_dir}/src/s/app.css"
 readonly reports_dir="${base_dir}/reports"
 readonly playwright_dir="${base_dir}/.playwright"
 readonly lhci_dir="${base_dir}/.lighthouseci"
 
 rm -rf "${build_dir}" \
-  "${prettier_cache_file}" \
-  "${ts_build_info_file}" \
   "${eslint_cache_dir}" \
+  "${prettier_cache_dir}" \
+  "${ts_build_cache_dir}" \
   "${generated_app_css_file}" \
   "${reports_dir}" \
   "${playwright_dir}" \
