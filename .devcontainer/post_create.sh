7d282dc289e504f6742bb13973642c20a2ebc9c1
Sebastian Davids <sdavids@gmx.de>
2025-05-24 17:51:32 +0200

    improve devcontainer setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,9 +3,11 @@
 # SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
 # SPDX-License-Identifier: Apache-2.0
 
-bash -i -c 'export SHELL=bash && curl -fsSL https://get.pnpm.io/install.sh | sh -'
+find hp -type d -name node_modules -exec rm -rf {} +
 
-bash -i -c 'cd hp && pnpm env use --global "$(cat .nvmrc)" && rm -rf node_modules && npm i --ignore-scripts=false --fund=false --audit=false && export DEBIAN_FRONTEND=noninteractive && npx playwright install --with-deps && npm cache clean --force >/dev/null 2>&1'
+curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION='10.8.0' sh -
+
+bash -i -c 'cd hp && pnpm env use --global "$(cat .nvmrc)" && npm i --ignore-scripts=true --fund=false --audit=false && export DEBIAN_FRONTEND=noninteractive && npx playwright install --with-deps && npm cache clean --force >/dev/null 2>&1'
 
 sudo apt-get autoremove -y
 sudo apt-get clean -y
