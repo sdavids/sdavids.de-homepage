09c0bf07fc30d803070834ca0d2b316237e2bdf4
Sebastian Davids <sdavids@gmx.de>
2026-03-07 16:25:30 +0100

    fix bug node not being downloaded in dev container
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,9 +5,15 @@
 
 find hp -type d -name node_modules -exec rm -rf {} +
 
-curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION="$(jq -r .devEngines.packageManager.version hp/package.json)" sh -
+pnpm_version="$(jq -r .devEngines.packageManager.version hp/package.json)"
+readonly pnpm_version
 
-bash -i -c "cd hp && pnpm env use --global $(perl <hp/pnpm-workspace.yaml -n -e '/useNodeVersion: (.*)/ && print $1') && pnpm install && export DEBIAN_FRONTEND=noninteractive && pnpm exec playwright install --with-deps"
+node_version="$(jq -r .devEngines.runtime.version hp/package.json)"
+readonly node_version
+
+curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION="${pnpm_version}" sh -
+
+bash -i -c "cd hp && pnpm env use --global ${node_version} && pnpm install && export DEBIAN_FRONTEND=noninteractive && pnpm exec playwright install --with-deps"
 
 sudo apt-get autoremove -y
 sudo apt-get clean -y
